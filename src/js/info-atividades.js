74746ac9b610e39d34a508d9c8077b8c098eda88
Davi-D18 <luiz.araujo@alunomicrolins.com.br>
2024-10-15 12:31:59 -0300

    Atv02: Adicionado adaptação para telas menores

--- src/js/info-atividades.js
+++ src/js/info-atividades.js
@@ -3,17 +3,17 @@ const description = "Imagem referente ao site dessa atividade"
 
 export const activities = [
   {
-    "title": "Atividade 01: Projeto Get Coffe",
-    "description": "Desenvolvimento de um site sobre café para atrair clientes",
-    "link": "./src/activities/atv01_projeto-coffe",
-    "image": "./src/assets/screenshots/get-coffe.png",
-    "alt": `${description}`
+    title: "Atividade 01: Projeto Get Coffe",
+    description: "Desenvolvimento de um site sobre café para atrair clientes",
+    link: "./src/activities/atv01_projeto-coffe",
+    image: "./src/assets/screenshots/get-coffe.png",
+    alt: `${description}`,
   },
   {
-    "title": "Atividade 02: Padrinhos Mágicos",
-    "description": "Criação de um site falando sobre os padrinhos mágicos",
-    "link": "./src/activities/atv02_padrinhos-magicos",
-    "image": "./src/assets/screenshots/padrinhos-magicos.png",
-    "alt": `${description}`
-  }
+    title: "Atividade 02: Padrinhos Mágicos",
+    description: "Criação de um site falando sobre os padrinhos mágicos",
+    link: "./src/activities/atv02_padrinhos-magicos",
+    image: "./src/assets/screenshots/padrinhos-magicos.png",
+    alt: `${description}`,
+  },
 ];
